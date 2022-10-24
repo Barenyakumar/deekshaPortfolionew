f90e65e44fd230485de1ead93465ef5d1d007284
BarenyaKumar <barenyakumar@gmail.com>
2022-10-24 13:52:30 +0530

    obfuscated js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,214 +1,280 @@
-/*==================== MENU SHOW Y HIDDEN ====================*/
-const navMenu = document.getElementById("nav-menu"),
-  navToggle = document.getElementById("nav-toggle"),
-  navClose = document.getElementById("nav-close")
-
-/*===== MENU SHOW =====*/
-/* Validate if constant exists */
-if (navToggle) {
-  navToggle.addEventListener("click", () => {
-    navMenu.classList.toggle("show-menu")
-  })
-}
-
-/*===== MENU HIDDEN =====*/
-/* Validate if constant exists */
-if (navClose) {
-  navClose.addEventListener("click", () => {
-    navMenu.classList.remove("show-menu")
-  })
-}
-
-/*==================== REMOVE MENU MOBILE ====================*/
-const navLink = document.querySelectorAll(".nav__link")
-
-function linkAction() {
-  const navMenu = document.getElementById("nav-menu")
-  // When we click on each nav__link, we remove the show-menu class
-  navMenu.classList.remove("show-menu")
-}
-navLink.forEach((n) => n.addEventListener("click", linkAction))
-
-/*==================== ACCORDION SKILLS ====================*/
-const skillsContent = document.getElementsByClassName("skills__content"),
-  skillsHeader = document.querySelectorAll(".skills__header")
-
-function toggleSkills() {
-  let itemClass = this.parentNode.className
-
-  for (i = 0; i < skillsContent.length; i++) {
-    skillsContent[i].className = "skills__content skills__close"
-  }
-  if (itemClass === "skills__content skills__close") {
-    this.parentNode.className = "skills__content skills__open"
-  }
-}
-
-skillsHeader.forEach((el) => {
-  el.addEventListener("click", toggleSkills)
-})
-
-/*==================== QUALIFICATION TABS ====================*/
-const tabs = document.querySelectorAll('[data-target]'),
-  tabContents= document.querySelectorAll('[data-content]')
-
-tabs.forEach(tab =>{
-  tab.addEventListener('click', () => {
-    const target = document.querySelector(tab.dataset.target)
-
-    tabContents.forEach (tabContent =>{
-      tabContent.classList.remove('qualification__active')
-    })
-
-    target.classList.add("qualification__active")
-
-    tab.forEach(tab => {
-      console.log("hi")
-      tab.classList.remove("qualification__active")
-    })
-    // for (const t of tab) {
-    //   t.classList.add("qualification__active")
-    // }
-    console.log(tab)
-    tab.classList.add('qualification__active')
-  })
-})
-/*==================== SERVICES MODAL ====================*/
-const modalViews = document.querySelectorAll(".services__modal"),
-  modalBtns = document.querySelectorAll(".services__button"),
-  modalCloses = document.querySelectorAll(".services__modal-close")
-
-let modal = function (modalClick) {
-  modalViews[modalClick].classList.add("active-modal")
-}
-
-modalBtns.forEach((modalBtn, i) => {
-  modalBtn.addEventListener("click", () => {
-    modal(i)
-  })
-})
-
-modalCloses.forEach((modalClose) => {
-  modalClose.addEventListener('click', () => {
-    modalViews.forEach((modalView) => {
-      modalView.classList.remove('active-modal')
-    })
-  })
-})
-
-/*==================== PORTFOLIO SWIPER  ====================*/
-let swiperPortfolio = new Swiper(".portfolio__container", {
-  cssMode: true,
-  loop: true,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    // clickableHold: true,
-  },
-  // mousewheel: true,
-  // keyboard: true,
-})
-
-
-/*==================== TESTIMONIAL ====================*/
-let swiperTestimonial = new Swiper(".testimonial__container", {
-  loop: true,
-  grabCursor: true,
-  spaceBetween:48,
-  
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    dynamicBullets: true,
-  },
-  breakpoints: {
-    568: {
-      slidesPerView: 2,
-    }
-  }
-})
-
-/*==================== SEND FORM MESSAGE ====================*/
-function SendMail() {
-  var params = {
-    from_name: document.getElementById("form__fullname").value,
-    email_id: document.getElementById("form__emailId").value,
-    message: document.getElementById("form__message").value
-  }
-  emailjs.send("service_gyr1vwj", "template_8gjf3od",params).then(function(res){
-    alert("Thanks for contacnting !");
-  })
-}
-
-
-/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
-const sections = document.querySelectorAll('section[id]')
-
-function scrollActive(){
-    const scrollY = window.pageYOffset
-
-    sections.forEach(current =>{
-        const sectionHeight = current.offsetHeight
-        const sectionTop = current.offsetTop - 50;
-        sectionId = current.getAttribute('id')
-
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-        }else{
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
-        }
-    })
-}
-window.addEventListener('scroll', scrollActive)
-
-/*==================== CHANGE BACKGROUND HEADER ====================*/ 
-function scrollHeader(){
-    const nav = document.getElementById('header')
-    // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
-    if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
-}
-window.addEventListener('scroll', scrollHeader)
-
-/*==================== SHOW SCROLL UP ====================*/ 
-function scrollUp(){
-    const scrollUp = document.getElementById('scroll-up');
-    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
-    if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
-}
-window.addEventListener('scroll', scrollUp)
-
-/*==================== DARK LIGHT THEME ====================*/ 
-const themeButton = document.getElementById('theme-button')
-const darkTheme = 'dark-theme'
-const iconTheme = 'uil-sun'
-
-// Previously selected topic (if user selected)
-const selectedTheme = localStorage.getItem('selected-theme')
-const selectedIcon = localStorage.getItem('selected-icon')
-
-// We obtain the current theme that the interface has by validating the dark-theme class
-const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
-
-// We validate if the user previously chose a topic
-if (selectedTheme) {
-  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-  themeButton.classList[selectedIcon === "uil-moon" ? "add" : "remove"](
-    iconTheme
+function _0x2536(_0x27122f, _0x360468) {
+  const _0x132cf7 = _0x132c()
+  return (
+    (_0x2536 = function (_0x2536e1, _0x4eaffa) {
+      _0x2536e1 = _0x2536e1 - 0x185
+      let _0xce4643 = _0x132cf7[_0x2536e1]
+      return _0xce4643
+    }),
+    _0x2536(_0x27122f, _0x360468)
   )
 }
-
-// Activate / deactivate the theme manually with the button
-themeButton.addEventListener('click', () => {
-    // Add or remove the dark / icon theme
-    document.body.classList.toggle(darkTheme)
-    themeButton.classList.toggle(iconTheme)
-    // We save the theme and the current icon that the user chose
-    localStorage.setItem('selected-theme', getCurrentTheme())
-    localStorage.setItem('selected-icon', getCurrentIcon())
+const _0xcabde1 = _0x2536
+;(function (_0x52250b, _0xe38b4a) {
+  const _0x29c983 = _0x2536,
+    _0x2b7a64 = _0x52250b()
+  while (!![]) {
+    try {
+      const _0x2b5c34 =
+        -parseInt(_0x29c983(0x19b)) / 0x1 +
+        parseInt(_0x29c983(0x196)) / 0x2 +
+        -parseInt(_0x29c983(0x1a7)) / 0x3 +
+        (parseInt(_0x29c983(0x1c7)) / 0x4) *
+          (-parseInt(_0x29c983(0x194)) / 0x5) +
+        -parseInt(_0x29c983(0x1c0)) / 0x6 +
+        parseInt(_0x29c983(0x1a2)) / 0x7 +
+        parseInt(_0x29c983(0x1c4)) / 0x8
+      if (_0x2b5c34 === _0xe38b4a) break
+      else _0x2b7a64["push"](_0x2b7a64["shift"]())
+    } catch (_0x86b60a) {
+      _0x2b7a64["push"](_0x2b7a64["shift"]())
+    }
+  }
+})(_0x132c, 0xd8c30)
+const navMenu = document["getElementById"](_0xcabde1(0x1aa)),
+  navToggle = document[_0xcabde1(0x185)](_0xcabde1(0x199)),
+  navClose = document[_0xcabde1(0x185)](_0xcabde1(0x1b4))
+navToggle &&
+  navToggle["addEventListener"](_0xcabde1(0x1b9), () => {
+    const _0x3836a4 = _0xcabde1
+    navMenu[_0x3836a4(0x1c2)][_0x3836a4(0x19c)]("show-menu")
+  })
+navClose &&
+  navClose["addEventListener"](_0xcabde1(0x1b9), () => {
+    const _0x548065 = _0xcabde1
+    navMenu[_0x548065(0x1c2)][_0x548065(0x1be)](_0x548065(0x1a0))
+  })
+const navLink = document[_0xcabde1(0x1af)](_0xcabde1(0x1a5))
+function linkAction() {
+  const _0x4f626c = _0xcabde1,
+    _0x41160c = document[_0x4f626c(0x185)](_0x4f626c(0x1aa))
+  _0x41160c["classList"]["remove"](_0x4f626c(0x1a0))
+}
+navLink[_0xcabde1(0x1a8)]((_0x218947) =>
+  _0x218947[_0xcabde1(0x186)](_0xcabde1(0x1b9), linkAction)
+)
+const skillsContent = document["getElementsByClassName"](_0xcabde1(0x195)),
+  skillsHeader = document["querySelectorAll"](".skills__header")
+function toggleSkills() {
+  const _0x412645 = _0xcabde1
+  let _0x2bbd5d = this[_0x412645(0x1b2)][_0x412645(0x1b0)]
+  for (i = 0x0; i < skillsContent[_0x412645(0x1b5)]; i++) {
+    skillsContent[i][_0x412645(0x1b0)] = _0x412645(0x1c1)
+  }
+  _0x2bbd5d === "skills__content\x20skills__close" &&
+    (this["parentNode"][_0x412645(0x1b0)] = _0x412645(0x198))
+}
+skillsHeader[_0xcabde1(0x1a8)]((_0x4d0620) => {
+  const _0x76e14b = _0xcabde1
+  _0x4d0620[_0x76e14b(0x186)]("click", toggleSkills)
+})
+function _0x132c() {
+  const _0x748a9b = [
+    "log",
+    "qualification__active",
+    "show-scroll",
+    "querySelectorAll",
+    "className",
+    "scrollY",
+    "parentNode",
+    "body",
+    "nav-close",
+    "length",
+    "uil-moon",
+    "form__emailId",
+    "scroll-up",
+    "click",
+    "setItem",
+    "selected-theme",
+    "send",
+    ".services__button",
+    "remove",
+    "service_gyr1vwj",
+    "9339174TpqCpK",
+    "skills__content\x20skills__close",
+    "classList",
+    ".portfolio__container",
+    "25383296OyJqmw",
+    "offsetTop",
+    "scroll-header",
+    "8HXNsBY",
+    "getElementById",
+    "addEventListener",
+    ".services__modal-close",
+    "target",
+    "offsetHeight",
+    "querySelector",
+    "scroll",
+    "getItem",
+    "add",
+    ".swiper-pagination",
+    "form__fullname",
+    "getAttribute",
+    ".nav__menu\x20a[href*=",
+    "dark-theme",
+    "dark",
+    "48355qdEmrH",
+    "skills__content",
+    "451236WXgpPS",
+    "uil-sun",
+    "skills__content\x20skills__open",
+    "nav-toggle",
+    ".testimonial__container",
+    "255174KgGmNF",
+    "toggle",
+    "light",
+    "active-modal",
+    "header",
+    "show-menu",
+    "active-link",
+    "2272424EnqTdl",
+    "form__message",
+    "selected-icon",
+    ".nav__link",
+    "contains",
+    "3012783ECkxlE",
+    "forEach",
+    "dataset",
+    "nav-menu",
+    "value",
+  ]
+  _0x132c = function () {
+    return _0x748a9b
+  }
+  return _0x132c()
+}
+const tabs = document["querySelectorAll"]("[data-target]"),
+  tabContents = document[_0xcabde1(0x1af)]("[data-content]")
+tabs[_0xcabde1(0x1a8)]((_0x3a7ec2) => {
+  _0x3a7ec2["addEventListener"]("click", () => {
+    const _0x18172c = _0x2536,
+      _0xba6f1e = document[_0x18172c(0x18a)](
+        _0x3a7ec2[_0x18172c(0x1a9)][_0x18172c(0x188)]
+      )
+    tabContents["forEach"]((_0xa44cb9) => {
+      const _0x2fe2f8 = _0x18172c
+      _0xa44cb9[_0x2fe2f8(0x1c2)][_0x2fe2f8(0x1be)](_0x2fe2f8(0x1ad))
+    }),
+      _0xba6f1e[_0x18172c(0x1c2)][_0x18172c(0x18d)](_0x18172c(0x1ad)),
+      _0x3a7ec2[_0x18172c(0x1a8)]((_0x1956e8) => {
+        const _0x73bdae = _0x18172c
+        console[_0x73bdae(0x1ac)]("hi"),
+          _0x1956e8[_0x73bdae(0x1c2)][_0x73bdae(0x1be)](_0x73bdae(0x1ad))
+      }),
+      console[_0x18172c(0x1ac)](_0x3a7ec2),
+      _0x3a7ec2[_0x18172c(0x1c2)][_0x18172c(0x18d)](_0x18172c(0x1ad))
+  })
+})
+const modalViews = document[_0xcabde1(0x1af)](".services__modal"),
+  modalBtns = document[_0xcabde1(0x1af)](_0xcabde1(0x1bd)),
+  modalCloses = document[_0xcabde1(0x1af)](_0xcabde1(0x187))
+let modal = function (_0x540c68) {
+  const _0x539a2f = _0xcabde1
+  modalViews[_0x540c68][_0x539a2f(0x1c2)][_0x539a2f(0x18d)](_0x539a2f(0x19e))
+}
+modalBtns["forEach"]((_0x585fae, _0x16f386) => {
+  _0x585fae["addEventListener"]("click", () => {
+    modal(_0x16f386)
+  })
+}),
+  modalCloses[_0xcabde1(0x1a8)]((_0x265a79) => {
+    const _0x62fbae = _0xcabde1
+    _0x265a79[_0x62fbae(0x186)](_0x62fbae(0x1b9), () => {
+      const _0x5e5259 = _0x62fbae
+      modalViews[_0x5e5259(0x1a8)]((_0x311517) => {
+        const _0x43cc89 = _0x5e5259
+        _0x311517[_0x43cc89(0x1c2)]["remove"](_0x43cc89(0x19e))
+      })
+    })
+  })
+let swiperPortfolio = new Swiper(_0xcabde1(0x1c3), {
+    cssMode: !![],
+    loop: !![],
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: { el: ".swiper-pagination", clickable: !![] },
+  }),
+  swiperTestimonial = new Swiper(_0xcabde1(0x19a), {
+    loop: !![],
+    grabCursor: !![],
+    spaceBetween: 0x30,
+    pagination: { el: _0xcabde1(0x18e), clickable: !![], dynamicBullets: !![] },
+    breakpoints: { 0x238: { slidesPerView: 0x2 } },
+  })
+function SendMail() {
+  const _0x1d0e7e = _0xcabde1
+  var _0x535869 = {
+    from_name: document[_0x1d0e7e(0x185)](_0x1d0e7e(0x18f))[_0x1d0e7e(0x1ab)],
+    email_id: document[_0x1d0e7e(0x185)](_0x1d0e7e(0x1b7))[_0x1d0e7e(0x1ab)],
+    message: document[_0x1d0e7e(0x185)](_0x1d0e7e(0x1a3))["value"],
+  }
+  emailjs[_0x1d0e7e(0x1bc)](_0x1d0e7e(0x1bf), "template_8gjf3od", _0x535869)[
+    "then"
+  ](function (_0x3754e3) {
+    alert("Thanks\x20for\x20contacnting\x20!")
+  })
+}
+const sections = document[_0xcabde1(0x1af)]("section[id]")
+function scrollActive() {
+  const _0x2f57b5 = _0xcabde1,
+    _0xf467f9 = window["pageYOffset"]
+  sections[_0x2f57b5(0x1a8)]((_0x28c79e) => {
+    const _0x38e28f = _0x2f57b5,
+      _0x5d2565 = _0x28c79e[_0x38e28f(0x189)],
+      _0x56ea98 = _0x28c79e[_0x38e28f(0x1c5)] - 0x32
+    ;(sectionId = _0x28c79e[_0x38e28f(0x190)]("id")),
+      _0xf467f9 > _0x56ea98 && _0xf467f9 <= _0x56ea98 + _0x5d2565
+        ? document[_0x38e28f(0x18a)](_0x38e28f(0x191) + sectionId + "]")[
+            "classList"
+          ][_0x38e28f(0x18d)](_0x38e28f(0x1a1))
+        : document["querySelector"](".nav__menu\x20a[href*=" + sectionId + "]")[
+            _0x38e28f(0x1c2)
+          ][_0x38e28f(0x1be)](_0x38e28f(0x1a1))
+  })
+}
+window["addEventListener"]("scroll", scrollActive)
+function scrollHeader() {
+  const _0xd81462 = _0xcabde1,
+    _0x51e9ce = document[_0xd81462(0x185)](_0xd81462(0x19f))
+  if (this[_0xd81462(0x1b1)] >= 0x50)
+    _0x51e9ce[_0xd81462(0x1c2)][_0xd81462(0x18d)](_0xd81462(0x1c6))
+  else _0x51e9ce[_0xd81462(0x1c2)]["remove"](_0xd81462(0x1c6))
+}
+window[_0xcabde1(0x186)](_0xcabde1(0x18b), scrollHeader)
+function scrollUp() {
+  const _0x4577fa = _0xcabde1,
+    _0x16e386 = document[_0x4577fa(0x185)](_0x4577fa(0x1b8))
+  if (this["scrollY"] >= 0x230)
+    _0x16e386["classList"][_0x4577fa(0x18d)]("show-scroll")
+  else _0x16e386[_0x4577fa(0x1c2)][_0x4577fa(0x1be)](_0x4577fa(0x1ae))
+}
+window[_0xcabde1(0x186)](_0xcabde1(0x18b), scrollUp)
+const themeButton = document[_0xcabde1(0x185)]("theme-button"),
+  darkTheme = _0xcabde1(0x192),
+  iconTheme = _0xcabde1(0x197),
+  selectedTheme = localStorage[_0xcabde1(0x18c)]("selected-theme"),
+  selectedIcon = localStorage[_0xcabde1(0x18c)]("selected-icon"),
+  getCurrentTheme = () =>
+    document[_0xcabde1(0x1b3)][_0xcabde1(0x1c2)][_0xcabde1(0x1a6)](darkTheme)
+      ? _0xcabde1(0x193)
+      : _0xcabde1(0x19d),
+  getCurrentIcon = () =>
+    themeButton[_0xcabde1(0x1c2)]["contains"](iconTheme)
+      ? _0xcabde1(0x1b6)
+      : _0xcabde1(0x197)
+selectedTheme &&
+  (document[_0xcabde1(0x1b3)][_0xcabde1(0x1c2)][
+    selectedTheme === _0xcabde1(0x193) ? _0xcabde1(0x18d) : _0xcabde1(0x1be)
+  ](darkTheme),
+  themeButton["classList"][
+    selectedIcon === _0xcabde1(0x1b6) ? _0xcabde1(0x18d) : _0xcabde1(0x1be)
+  ](iconTheme))
+themeButton[_0xcabde1(0x186)](_0xcabde1(0x1b9), () => {
+  const _0x594e18 = _0xcabde1
+  document[_0x594e18(0x1b3)][_0x594e18(0x1c2)][_0x594e18(0x19c)](darkTheme),
+    themeButton["classList"][_0x594e18(0x19c)](iconTheme),
+    localStorage[_0x594e18(0x1ba)](_0x594e18(0x1bb), getCurrentTheme()),
+    localStorage[_0x594e18(0x1ba)](_0x594e18(0x1a4), getCurrentIcon())
 })
--- gallery/js/main.js
+++ gallery/js/main.js
@@ -1,53 +1,91 @@
-/*=============== SWIPER JS GALLERY ===============*/
-let swiperCards1 = new Swiper(".gallery-cards1", {
-  loop: true,
-  loopedSlides: 5,
-  cssMode: true,
-  effect: "fade",
-})
-
-let swiperThumbs1 = new Swiper(".gallery-thumbs1", {
-  loop: true,
-  loopedSlides: 5,
-  slidesPerView: 3,
-  centeredSlides: true,
-  slideToClickedSlide: true,
-  pagination: {
-    el: ".swiper-pagination",
-    type: "fraction",
-  },
-  // navigation: {
-  //   nextEl: ".swiper-button-next1",
-  //   prevEl: ".swiper-button-prev1",
-  // },
-})
-
-swiperThumbs1.controller.control = swiperCards1
-
-
-
-/*=============== SWIPER JS GALLERY ===============*/
-let swiperCards2 = new Swiper(".gallery-cards2", {
-  loop: true,
-  loopedSlides: 5,
-  cssMode: true,
-  effect: "fade",
-})
-
-let swiperThumbs2 = new Swiper(".gallery-thumbs2", {
-  loop: true,
-  loopedSlides: 5,
-  slidesPerView: 3,
-  centeredSlides: true,
-  slideToClickedSlide: true,
-  pagination: {
-    el: ".swiper-pagination",
-    type: "fraction",
-  },
-  // navigation: {
-  //   nextEl: ".swiper-button-next1",
-  //   prevEl: ".swiper-button-prev",
-  // },
-})
-
-swiperThumbs2.controller.control = swiperCards2
+const _0x2bccf5 = _0x144b
+;(function (_0x3867b9, _0x32dbc6) {
+  const _0x88a7a0 = _0x144b,
+    _0x1849b5 = _0x3867b9()
+  while (!![]) {
+    try {
+      const _0x28cfb4 =
+        -parseInt(_0x88a7a0(0x150)) / 0x1 +
+        (-parseInt(_0x88a7a0(0x146)) / 0x2) *
+          (parseInt(_0x88a7a0(0x152)) / 0x3) +
+        (-parseInt(_0x88a7a0(0x145)) / 0x4) *
+          (-parseInt(_0x88a7a0(0x144)) / 0x5) +
+        -parseInt(_0x88a7a0(0x14c)) / 0x6 +
+        -parseInt(_0x88a7a0(0x14e)) / 0x7 +
+        (-parseInt(_0x88a7a0(0x151)) / 0x8) *
+          (parseInt(_0x88a7a0(0x148)) / 0x9) +
+        parseInt(_0x88a7a0(0x154)) / 0xa
+      if (_0x28cfb4 === _0x32dbc6) break
+      else _0x1849b5["push"](_0x1849b5["shift"]())
+    } catch (_0xb2a475) {
+      _0x1849b5["push"](_0x1849b5["shift"]())
+    }
+  }
+})(_0x47c1, 0xf3153)
+let swiperCards1 = new Swiper(_0x2bccf5(0x143), {
+    loop: !![],
+    loopedSlides: 0x5,
+    cssMode: !![],
+    effect: _0x2bccf5(0x147),
+  }),
+  swiperThumbs1 = new Swiper(_0x2bccf5(0x14f), {
+    loop: !![],
+    loopedSlides: 0x5,
+    slidesPerView: 0x3,
+    centeredSlides: !![],
+    slideToClickedSlide: !![],
+    pagination: { el: ".swiper-pagination", type: _0x2bccf5(0x14d) },
+  })
+swiperThumbs1[_0x2bccf5(0x14b)]["control"] = swiperCards1
+let swiperCards2 = new Swiper(_0x2bccf5(0x153), {
+    loop: !![],
+    loopedSlides: 0x5,
+    cssMode: !![],
+    effect: _0x2bccf5(0x147),
+  }),
+  swiperThumbs2 = new Swiper(_0x2bccf5(0x14a), {
+    loop: !![],
+    loopedSlides: 0x5,
+    slidesPerView: 0x3,
+    centeredSlides: !![],
+    slideToClickedSlide: !![],
+    pagination: { el: ".swiper-pagination", type: "fraction" },
+  })
+function _0x47c1() {
+  const _0x1dd556 = [
+    "controller",
+    "10359036yncsAO",
+    "fraction",
+    "9631825rMtWoG",
+    ".gallery-thumbs1",
+    "1134280ukpKsK",
+    "2056OuJdos",
+    "21GMPQQk",
+    ".gallery-cards2",
+    "64058240ifKQHq",
+    ".gallery-cards1",
+    "65445RiqNOw",
+    "444puZsuj",
+    "503938pckOPf",
+    "fade",
+    "30204KHzuPi",
+    "control",
+    ".gallery-thumbs2",
+  ]
+  _0x47c1 = function () {
+    return _0x1dd556
+  }
+  return _0x47c1()
+}
+function _0x144b(_0x42a3c2, _0x21addd) {
+  const _0x47c107 = _0x47c1()
+  return (
+    (_0x144b = function (_0x144b8a, _0x3aa773) {
+      _0x144b8a = _0x144b8a - 0x143
+      let _0x486e9b = _0x47c107[_0x144b8a]
+      return _0x486e9b
+    }),
+    _0x144b(_0x42a3c2, _0x21addd)
+  )
+}
+swiperThumbs2[_0x2bccf5(0x14b)][_0x2bccf5(0x149)] = swiperCards2
